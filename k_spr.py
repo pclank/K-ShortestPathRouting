54f492b402482f888e82c2ff9357f1b3fe30b28a
pclank <31415965+pclank@users.noreply.github.com>
2020-12-15 20:15:19 +0200

    Initial commit.

---
+++ k_spr.py
@@ -0,0 +1,12 @@
+# Import Libraries
+
+import numpy as np
+
+# Define Network Topology as Array - Matrix
+network = np.array([[0, 0, 5, 0, 0],
+                    [0, 0, 0, 3, 7],
+                    [5, 0, 0, 1, 0],
+                    [0, 3, 1, 0, 1],
+                    [0, 7, 0, 1, 0]])
+
+
